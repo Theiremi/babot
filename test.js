4722edfd2b743b8434bf3fb2ab13cee05ead54b2
Theiremi <84141518+Theiremi@users.noreply.github.com>
2023-03-15 13:01:04 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-let test_code = "https://discord.com/api/webhooks/1085258130016972942/9H-6uENf0f5iaVqUWkKPgPmGBUeGpmTAD_8vYRYh0JOuWHN8qmgrVZSJxH1RDaLECN0H"
+let test_code = "https://discord.com/api/webhooks/1085318226252144710/nNfyAWloORmDVEjlOmTS29on0nKhBQx_aiygUuTfByNODZcPAEtERmw0HZ1B5cd01Jvh"
